675d60100493305979823ac4cc10f7638d8b281a
Eric <Eroginek@gmail.com>
2024-05-01 18:37:45 -0400

    Added rank-score characteristic plotting for fusion regression

--- run_regression.py
+++ run_regression.py
@@ -11,7 +11,7 @@ DATASET = 'HTS'
 if not os.path.exists(f'./results/'): os.mkdir(f'./results/')
 if not os.path.exists(f'./results/{ROOT}'): os.mkdir(f'./results/{ROOT}')
 if not os.path.exists(f'./results/{ROOT}/{DATASET}'): os.mkdir(f'./results/{ROOT}/{DATASET}')
-OUTPATH = f'./results/{ROOT}/{DATASET}/regression_score_combination.PNG'
+OUTPATH = f'./results/{ROOT}/{DATASET}'
 
 score_data, ground_truth = get_outputs(f"{ROOT}/{DATASET}")
 
@@ -20,7 +20,7 @@ G = list(ground_truth['0'])
 
 infuse_regression = InFusionRegression(T, weighting_schemes=['AC', 'WCDS'])
 score_combinations, rank_combinations = infuse_regression.predict()
-
+print(score_combinations)
 data = []
 
 # Collect data from score combinations
@@ -61,6 +61,31 @@ ax[1].tick_params(axis='x', rotation=90)
 
 # Improve layout and show the plot
 plt.tight_layout()
-plt.savefig(OUTPATH)
+plt.savefig(f'{OUTPATH}/regression_score_combination.PNG')
+# plt.show()
+# plt.close()
+
+# Plot RSC
+import torch
+
+def normalize_and_sort_tensor(tensor):
+    """ Normalizes a tensor to range [0, 1] and sorts it in descending order. """
+    normalized_tensor = tensor / torch.max(tensor)
+    sorted_tensor, _ = torch.sort(normalized_tensor, descending=True)
+    return sorted_tensor
+
+# Normalize and plot each tensor
+plt.figure(figsize=(12, 6))
+for model_name, tensor in score_combinations.items():
+    normalized_tensor = normalize_and_sort_tensor(tensor)
+    plt.plot(normalized_tensor.numpy(), label=model_name)  # Convert tensor to numpy array for plotting
+
+# Customize the plot
+plt.title('Rank-Score Characteristic')
+plt.xlabel('Ranks')
+plt.ylabel('Normalized Scores')
+plt.legend(title='Models', loc='upper center', bbox_to_anchor=(1.15, 1))
+plt.grid(True)
+plt.tight_layout()
+plt.savefig(f'{OUTPATH}/rank_score_characteristics.PNG')
 plt.show()
-plt.close()
